e156c22dc24e73cd43aaac7b156794c52413ac2d
Ricardo Vilhena <a79305@ualg.pt>
2023-11-20 15:41:23 +0000

    Eliminei ficheiros clientes e copiei o código para o ficheiro cliente.py

--- src/clientes.py
+++ src/clientes.py
@@ -2,10 +2,42 @@ from io_terminal import imprime_lista
 
 nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome clientes-*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro clientes.py existente, deve apagar
-# todos os outros ficheiros clientes-*.py, e inclusive estes comentários
+def imprime_lista_de_clientes(lista_de_clientes):
+    """TODO: documentação"""
 
-# ...
+    #TODO: Implementar esta função
+    # ...
+
+def cria_novo_cliente():
+    # Inicialize um dicionário vazio para armazenar as informações do cliente
+    novo_cliente = {}
+
+    # Solicite as informações do cliente ao usuário
+    novo_cliente["nome"] = input("Nome do cliente: ")
+    novo_cliente["nif"] = input("NIF do cliente: ")
+    novo_cliente["email"] = input("E-mail do cliente: ")
+    novo_cliente["telefone"] = input("Telefone do cliente: ")
+
+    # Retorne o dicionário com as informações do novo cliente
+    return novo_cliente
+
+nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
+
+def imprime_lista_de_clientes(lista_de_clientes):
+    """
+    Imprime a lista de clientes.
+
+    :param lista_de_clientes: Uma lista de dicionários, onde cada dicionário contém informações de um cliente.
+    """
+    if not lista_de_clientes:
+        print("A lista de clientes está vazia.")
+        return
+
+    print("Lista de Clientes:")
+    for cliente in lista_de_clientes:
+        print(f"Nome: {cliente['nome']}")
+        print(f"NIF: {cliente['nif']}")
+        print(f"E-mail: {cliente['email']}")
+        print(f"Telemovel: {cliente['telemovel']}")
+        print("-" * 20)  # Linha separadora entre os clientes
+
