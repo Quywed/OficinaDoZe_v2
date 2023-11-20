e6683c4f3acb77684ee77cf2a592bdb52645daea
Ricardo Vilhena <a79305@ualg.pt>
2023-11-20 18:12:44 +0000

    Resolvi listagem de cliente e ordem atributos da tabela cliente

--- src/clientes.py
+++ src/clientes.py
@@ -1,34 +1,31 @@
-from io_terminal import imprime_lista
+import sqlite3
 
-nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
+# Função para inserir um novo cliente
+def inserir_novo_cliente(conn):
 
-def imprime_lista_de_clientes(lista_de_clientes):
-    """TODO: documentação"""
+    cursor = conn.cursor()
 
-    #TODO: Implementar esta função
-    # ...
+    nome = input("Nome do cliente: ")
+    nif = input("NIF do cliente: ")
+    email = input("E-mail do cliente: ")
+    telefone = input("Telefone do cliente: ")
 
-def cria_novo_cliente():
-    # Inicialize um dicionário vazio para armazenar as informações do cliente
-    novo_cliente = {}
+    cursor.execute('''
+        INSERT INTO clientes (nif, name, email, telefone)
+        VALUES (?, ?, ?, ?)
+    ''', (nif, nome, email, telefone))
+    
+    conn.commit()
+    conn.close()
 
-    # Solicite as informações do cliente ao usuário
-    novo_cliente["nome"] = input("Nome do cliente: ")
-    novo_cliente["nif"] = input("NIF do cliente: ")
-    novo_cliente["email"] = input("E-mail do cliente: ")
-    novo_cliente["telefone"] = input("Telefone do cliente: ")
+# Função para imprimir a lista de clientes
+def imprime_lista_de_clientes(conn):
 
-    # Retorne o dicionário com as informações do novo cliente
-    return novo_cliente
+    cursor = conn.cursor()
 
-nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
+    cursor.execute('SELECT * FROM clientes')
+    lista_de_clientes = cursor.fetchall()
 
-def imprime_lista_de_clientes(lista_de_clientes):
-    """
-    Imprime a lista de clientes.
-
-    :param lista_de_clientes: Uma lista de dicionários, onde cada dicionário contém informações de um cliente.
-    """
     if not lista_de_clientes:
         print("A lista de clientes está vazia.")
         return
