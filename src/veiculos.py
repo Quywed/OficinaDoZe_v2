3d070476b053da117a7a84c4907ab1589778a5fe
Pedro <a79297@ualg.pt>
2023-11-20 18:01:20 +0000

    Adicionei as funcoes base de veiculos

--- src/veiculos.py
+++ src/veiculos.py
@@ -1,28 +1,73 @@
-from io_terminal import imprime_lista
+import sqlite3  
 
-nome_ficheiro_lista_de_veiculos = "lista_de_veiculos.pk"
+def insert_veiculo(conn):
+
+    cursor = conn.cursor()
+    matricula = input("Matricula: ")
+    modelo = input("Modelo: ")
+    marca = input("Marca: ")
+    ano = int(input("Ano: "))
+    km = int(input("Km: "))
+    nome_motor = input("Nome do Motor: ")
+    cliente_nif = int(input("NIF Cliente: "))
 
 
-def cria_novo_veiculo():
-    """Pede ao utilizador para introduzir um novo veiculo
+    try:
+        cursor.execute('''
+            INSERT INTO Veiculos (matricula, modelo, marca, ano, km, nome_motor, cliente_nif)
+            VALUES (?, ?, ?, ?, ?, ?, ?);
+        ''', (matricula, modelo, marca, ano, km, nome_motor, cliente_nif))
 
-    :return: dicionario com um veiculo na forma
-        {"marca": <<marca>>, "matricula": <<matricula>>, ...}
-    """
+        conn.commit()
+        print("Veiculo inserido com sucesso.")
+    except sqlite3.Error as e:
+        print(f"Erro no insert do veiculo: {e}")
+        conn.rollback()
 
-    marca = input("marca? ")
-    matricula = input("matricula? ").upper()
-    # TODO: Pedir o resto dos dados do veiculo, e não esquecer de os guardar no dicionario
-    # ...
+    conn = sqlite3.connect('src/oficina.db')
+    conn.close()
 
-    veiculo = {"marca": marca,
-               "matricula": matricula}
 
-    return veiculo
 
-def imprime_lista_de_veiculos(lista_de_veiculos):
-    """TODO: documentação"""
+def select_veiculo_cliente(conn, client_nif):
+    try:
+        cursor = conn.cursor()
 
-    imprime_lista(cabecalho="Lista de Veiculos", lista=lista_de_veiculos)
+        cursor.execute('''
+            SELECT * FROM Veiculos WHERE cliente_nif = ?;
+        ''', (client_nif,))
+
+        cars = cursor.fetchall()
+
+        if cars:
+            print(f"Carros do Cliente com o NIF:{client_nif}:")
+            for car in cars:
+                print(f"Matricula: {car[0]}, Modelo: {car[1]}, Marca: {car[2]}, Ano: {car[3]}, KM: {car[4]}, Nome do Motor: {car[5]}")
+        else:
+            print(f"O Cliente com o NIF: {client_nif} não tem carros")
+
+    except sqlite3.Error as e:
+        print(f"Erro ao procurar os carros: {e}")
+
+
+def list_veiculos(conn):
+    try:
+        cursor = conn.cursor()
+
+        cursor.execute('''
+            SELECT * FROM Veiculos;
+        ''')
+
+        veiculos = cursor.fetchall()
+
+        if veiculos:
+            print("Todos os Veiculos:")
+            for veiculo in veiculos:
+                print(f"Matricula: {veiculo[0]}, Modelo: {veiculo[1]}, Marca: {veiculo[2]}, Ano: {veiculo[3]}, KM: {veiculo[4]}, Nome do Motor: {veiculo[5]}, Cliente NIF: {veiculo[6]}")
+        else:
+            print("Não ouve veiculos encontrados na Tabela")
+
+    except sqlite3.Error as e:
+        print(f"Erro: {e}")
 
 
