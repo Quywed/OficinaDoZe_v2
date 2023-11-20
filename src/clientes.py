dd6a18d4538d84ceb2e95c74c47e9ff88e1431e8
Ricardo Vilhena <a79305@ualg.pt>
2023-11-20 17:11:54 +0000

    Adaptei as funções de cliente para o uso da base de dados sql lite

--- src/clientes.py
+++ src/clientes.py
@@ -1,43 +1,57 @@
-from io_terminal import imprime_lista
+import sqlite3
 
-nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
+# Função para criar a tabela de clientes
+def criar_tabela_clientes():
+    conn = sqlite3.connect('src/oficina.db')  # Conecta ao banco de dados (cria se não existir)
+    cursor = conn.cursor()
 
-def imprime_lista_de_clientes(lista_de_clientes):
-    """TODO: documentação"""
+    # Cria uma tabela de clientes
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS cliente (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            nif TEXT NOT NULL,
+            email TEXT NOT NULL,
+            telefone TEXT NOT NULL
+        )
+    ''')
+    
+    conn.commit()  # Salva as alterações
+    conn.close()   # Fecha a conexão
 
-    #TODO: Implementar esta função
-    # ...
+# Função para inserir um novo cliente
+def inserir_novo_cliente(cliente):
+    conn = sqlite3.connect('src/oficina.db')
+    cursor = conn.cursor()
 
-def cria_novo_cliente():
-    # Inicialize um dicionário vazio para armazenar as informações do cliente
-    novo_cliente = {}
+    # Insere um novo cliente na tabela
+    cursor.execute('''
+        INSERT INTO cliente (nome, nif, email, telefone)
+        VALUES (?, ?, ?, ?)
+    ''', (cliente['nome'], cliente['nif'], cliente['email'], cliente['telefone']))
+    
+    conn.commit()
+    conn.close()
 
-    # Solicite as informações do cliente ao usuário
-    novo_cliente["nome"] = input("Nome do cliente: ")
-    novo_cliente["nif"] = input("NIF do cliente: ")
-    novo_cliente["email"] = input("E-mail do cliente: ")
-    novo_cliente["telefone"] = input("Telefone do cliente: ")
+# Função para imprimir a lista de clientes
+def imprime_lista_de_clientes():
+    conn = sqlite3.connect('src/oficina.db')
+    cursor = conn.cursor()
 
-    # Retorne o dicionário com as informações do novo cliente
-    return novo_cliente
+    cursor.execute('SELECT * FROM clientes')
+    lista_de_clientes = cursor.fetchall()
 
-nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
-
-def imprime_lista_de_clientes(lista_de_clientes):
-    """
-    Imprime a lista de clientes.
-
-    :param lista_de_clientes: Uma lista de dicionários, onde cada dicionário contém informações de um cliente.
-    """
     if not lista_de_clientes:
         print("A lista de clientes está vazia.")
         return
 
     print("Lista de Clientes:")
     for cliente in lista_de_clientes:
-        print(f"Nome: {cliente['nome']}")
-        print(f"NIF: {cliente['nif']}")
-        print(f"E-mail: {cliente['email']}")
-        print(f"Telemovel: {cliente['telemovel']}")
-        print("-" * 20)  # Linha separadora entre os clientes
+        print(f"ID: {cliente[0]}")
+        print(f"Nome: {cliente[1]}")
+        print(f"NIF: {cliente[2]}")
+        print(f"E-mail: {cliente[3]}")
+        print(f"Telefone: {cliente[4]}")
+        print("-" * 20)
 
+    conn.close()
