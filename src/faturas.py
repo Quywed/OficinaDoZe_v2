c81a3eda25a0bda934286870b44fc9e485edbacd
gonmarques <gonmarques@deloitte.pt>
2023-11-24 10:42:55 +0000

    criar função nova fatura

--- src/faturas.py
+++ src/faturas.py
@@ -4,12 +4,17 @@ from io_terminal import imprime_lista, pergunta_id
 
 nome_ficheiro_lista_de_faturas = "lista_de_faturas.pk"
 
+def cria_nova_fatura(cliente_nif, descricao, valor):
+    conn = sqlite3.connect('src/oficina.db')
+    cursor = conn.cursor()
 
-def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
+    data_criacao = datetime.now()
 
-    return 
+    cursor.execute("INSERT INTO faturas (cliente_nif, data_criacao, descricao, valor) VALUES (?, ?, ?, ?)",
+                   (cliente_nif, data_criacao, descricao, valor))
 
+    conn.commit()
+    conn.close()
 
-def imprime_lista_de_faturas(lista_de_faturas):
-
-    pass
+def imprime_lista_de_faturas():
+   pass
