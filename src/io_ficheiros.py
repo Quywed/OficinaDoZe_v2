7edb9e28b99a026408f6b832048ca231737a2777
Ricardo Vilhena <a79305@ualg.pt>
2023-11-24 10:19:50 +0000

    Delete e passagem do código para io_ficheiros do ficheiro io_lista_guarda_ficheiro

--- src/io_ficheiros.py
+++ src/io_ficheiros.py
@@ -6,10 +6,18 @@ from faturas import nome_ficheiro_lista_de_faturas
 from veiculos import nome_ficheiro_lista_de_veiculos
 
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome io_ficheiros*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro io_ficheiros.py existente, deve apagar
-# todos os outros ficheiros io_ficheiros-*.py, e inclusive estes comentários
+def guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_clientes, lista_de_faturas):
+    """TODO: documentação
 
-# ...
+    :param lista_de_clientes:
+    :param lista_de_veiculos:
+    :param lista_de_faturas:
+    """
+
+    op = input("Os dados nos ficheiros serão sobrepostos. Continuar (s/N)?")
+    if op in ['s', 'S']:
+        guarda_em_ficheiro(nome_ficheiro_lista_de_veiculos, lista_de_veiculos)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_clientes, lista_de_clientes)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_faturas, lista_de_faturas)
+    else:
+        print("Gravação cancelada...")
