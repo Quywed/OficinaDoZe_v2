c85230eca174c084246868471ec64b58e76a1b6f
Ricardo Vilhena <a79305@ualg.pt>
2023-11-25 19:21:31 +0000

    Tornar o ficheiro bd drops file em função

--- src/bd_DROPS_file.py
+++ src/bd_DROPS_file.py
@@ -1,21 +1,20 @@
 import sqlite3
 
-# CONEXAO
-conn = sqlite3.connect('src/oficina.db')
-cursor = conn.cursor()
+def dropping_bd():
+    # CONEXAO
+    conn = sqlite3.connect('src/oficina.db')
+    cursor = conn.cursor()
 
-# DROP TABLES
-cursor.execute("DROP TABLE IF EXISTS Veiculos;")
-cursor.execute("DROP TABLE IF EXISTS clientes;")
-cursor.execute("DROP TABLE IF EXISTS faturas;")
-cursor.execute("DROP TABLE IF EXISTS empregados;")
-
-
-# COMMIT AND CLOSE CONNECTION
-conn.commit()
-conn.close()
+    # DROP TABLES
+    cursor.execute("DROP TABLE IF EXISTS Veiculos;")
+    cursor.execute("DROP TABLE IF EXISTS clientes;")
+    cursor.execute("DROP TABLE IF EXISTS faturas;")
+    cursor.execute("DROP TABLE IF EXISTS empregados;")
 
 
+    # COMMIT AND CLOSE CONNECTION
+    conn.commit()
+    conn.close()
 
 
-
+dropping_bd()
