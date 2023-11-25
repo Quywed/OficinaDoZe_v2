052b846c3546835e552a1cef5dd74cac9d29669a
Ricardo Vilhena <a79305@ualg.pt>
2023-11-25 18:03:51 +0000

    Alteracoes a docstrings e adicao de docstrings aos ficheiros, depois de pull de alteracoes do branch teste.

---
+++ src/bd_TESTE_LOGIN.py
@@ -0,0 +1,100 @@
+import sqlite3
+import hashlib
+
+# CONEXAO
+conn = sqlite3.connect('src/oficina.db')
+cursor = conn.cursor()
+
+def hash_password(password):
+    """
+    Retorna o hash SHA-256 da senha fornecida.
+
+    :param password: A senha a ser criptografada.
+    :type password: str
+    :return: O hash SHA-256 da senha.
+    :rtype: str
+    """
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+def login(cursor, letra):
+    """
+    Realiza a autenticação de cliente ou empregado.
+
+    :param cursor: O cursor da base de dados.
+    :type cursor: sqlite3.Cursor
+    :param letra: A letra representando 'c' para cliente ou 'e' para empregado.
+    :type letra: str
+    :return: Verdadeiro se a autenticação for bem-sucedida, falso caso contrário.
+    :rtype: bool
+    """
+
+    # INPUTS
+    nif_input = input("|LOGIN|\n NIF: ")
+    password_input = input("Password: ")
+
+    hashed_password_input = hash_password(password_input)
+
+    if letra == 'c':
+        # CHECK CLIENT
+        cursor.execute("SELECT * FROM clientes WHERE nif = ? AND password_hash = ?", (nif_input, hashed_password_input))
+        client = cursor.fetchall()
+
+        if client:        
+            
+            
+            return True
+        else:
+            print("Cliente não foi encontrado ou introduziu a senha incorretamente.")
+            return False
+
+    elif letra == 'e':
+        # CHECK empregado
+        cursor.execute("SELECT * FROM empregados WHERE empregado_nif = ? AND password_hash = ?", (nif_input, hashed_password_input))
+        empregado = cursor.fetchall()
+ 
+        if empregado:
+
+
+
+            return True
+        else:
+            print("Empregado não foi encontrado ou introduziu a senha incorretamente.")
+            return False
+
+
+
+
+
+
+#MAIN DO TESTE
+choice = input("Opção: (c - cliente | e - empregado)")
+
+
+if choice == 'c':
+    cursor.execute("SELECT * FROM clientes;")
+    clientes = cursor.fetchall()
+
+    print("\nCLIENTES:")
+    for cliente in clientes:
+        print(cliente)
+
+    cliente_encontrado = login(cursor, choice)
+    if cliente_encontrado == True:
+        print ('funcões do cliente')
+
+elif choice == 'e':
+    cursor.execute("SELECT * FROM empregados;")
+    empregados = cursor.fetchall()
+
+    print("\nCLIENTES:")
+    for empregado in empregados:
+        print(empregado)
+
+
+    empregado_encontrado = login(cursor, choice)
+    if empregado_encontrado == True:
+        print('funções do empregado')
+
+
+    
---
+++ src/bd_file.py
@@ -0,0 +1,159 @@
+import sqlite3
+from datetime import datetime
+import random
+import hashlib
+
+def hash_password(password):
+    """
+    Retorna o hash SHA-256 da senha fornecida.
+
+    :param password: A senha a ser criptografada.
+    :type password: str
+    :return: O hash SHA-256 da senha.
+    :rtype: str
+    """
+    return hashlib.sha256(password.encode()).hexdigest()
+
+# CONEXAO
+conn = sqlite3.connect('src/oficina.db')
+
+# CURSOR PARA EXECUTAR QUERYS
+cursor = conn.cursor()
+
+# TABELA Veiculos
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Veiculos (
+        matricula TEXT PRIMARY KEY,
+        modelo TEXT,
+        marca TEXT,
+        ano INTEGER,
+        km INTEGER,
+        nome_motor TEXT,
+        cliente_nif INTEGER,
+        FOREIGN KEY (cliente_nif) REFERENCES clientes(nif)
+    );
+''')
+
+# TABELA clientes
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS clientes (
+        nif INTEGER PRIMARY KEY,
+        name TEXT,
+        email TEXT,
+        telefone TEXT,
+        password_hash TEXT
+    );
+''')
+
+# TABELA empregados
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS empregados (
+        empregado_nif INTEGER PRIMARY KEY,
+        name TEXT,
+        email TEXT,
+        password_hash TEXT
+    );
+''')
+
+# TABELA faturas
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS faturas (
+        id INTEGER PRIMARY KEY,
+        cliente_nif INTEGER,
+        data_criacao DATE DEFAULT CURRENT_DATE,
+        descricao TEXT,
+        valor REAL,
+        FOREIGN KEY (cliente_nif) REFERENCES clientes(nif)
+    );
+''')
+
+#-----------------------------------INSERTS RANDOM-----------------------------------------------
+
+veiculos_data = []
+clientes_data = []
+empregados_data = []
+faturas_data = []
+
+cliente_random_1 = random.randint(1000, 9999)
+cliente_random_2 = random.randint(1000, 9999)
+
+#INSERT EMPREGADOS-----------------------------------------------
+#INSERT EMPREGADOS-----------------------------------------------
+for e in range(2):
+    password_empregado = "password_empregado" 
+    password_hash_empregado = hashlib.sha256(password_empregado.encode()).hexdigest()
+    empregados_data.append((e + 1, f'Empregado{e}', f'empregado{e}@email.com', password_hash_empregado))
+
+cursor.executemany("INSERT INTO empregados VALUES (?, ?, ?, ?)", empregados_data)
+#---------------------------------------------------------------
+
+#INSERT CLIENTES-----------------------------------------------
+password_cliente = "password_cliente"
+password_hash_cliente = hashlib.sha256(password_cliente.encode()).hexdigest()
+
+#CLIENTE 1
+password_hash_cliente = hashlib.sha256(password_cliente.encode()).hexdigest()
+clientes_data.append((cliente_random_1,'Cliente1', 'cliente1@email.com', '123456789', password_hash_cliente))
+
+#CLIENTE 2 
+password_hash_cliente = hashlib.sha256(password_cliente.encode()).hexdigest()
+clientes_data.append((cliente_random_2,'Cliente2', 'cliente2@email.com', '987654321', password_hash_cliente))
+
+#--------------------------------------------------------------
+#INSERT MATRICULAS---------------------------------------------
+matricula_random_1 = "ABC_200"
+matricula_random_2 = "CBA_300"
+
+#MATRICULA 1
+veiculos_data.append((matricula_random_1, 'Modelo1', 'Marca1', 2020, 50000, 'Motor1', cliente_random_1))
+
+#MATRICULA 2
+veiculos_data.append((matricula_random_2, 'Modelo2', 'Marca2', 2020, 50000, 'Motor2', cliente_random_2))
+
+#--------------------------------------------------------------
+#INSERT FATURAS------------------------------------------------
+
+faturas_random_1 = 111
+faturas_random_2 = 222
+
+#FATURA 1
+faturas_data.append((faturas_random_1, cliente_random_1, datetime.now(), 'Servico1', 100.00))
+
+#FATURA 2
+faturas_data.append((faturas_random_2, cliente_random_2, datetime.now(), 'Servico2', 200.00))
+
+cursor.executemany("INSERT INTO Veiculos VALUES (?, ?, ?, ?, ?, ?, ?)", veiculos_data)
+cursor.executemany("INSERT INTO clientes VALUES (?, ?, ?, ?, ?)", clientes_data)
+cursor.executemany("INSERT INTO faturas VALUES (?, ?, ?, ?, ?)", faturas_data)
+
+# LISTAR (SELECT)
+cursor.execute("SELECT * FROM Veiculos;")
+veiculos = cursor.fetchall()
+
+print("\nVEICULOS:")
+for veiculo in veiculos:
+    print(veiculo)
+
+cursor.execute("SELECT * FROM clientes;")
+clientes = cursor.fetchall()
+
+print("\nCLIENTES:")
+for cliente in clientes:
+    print(cliente)
+
+cursor.execute("SELECT * FROM empregados;")
+empregados = cursor.fetchall()
+
+print("\nEMPREGADO:")
+for i in empregados:
+    print(i)
+
+cursor.execute("SELECT * FROM faturas;")
+faturas = cursor.fetchall()
+
+print("\nFATURAS:")
+for fatura in faturas:
+    print(fatura)
+
+conn.commit()  # COMMIT
+conn.close()   # FECHAR A CONEXAO
